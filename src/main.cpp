3670267135e3251ab2a527c68236e6bd0ca1d2f3
Macarena Oyague <macarena.oyague@utec.edu.pe>
2021-08-03 16:42:03 -0500

    Time

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,22 @@
 #include "queries.h"
+#include <chrono>
+
+using namespace std::chrono;
 
 int main () {
     RTree* rtree =  read_neighborhoods("../data/neighborhoods.txt");
     insert_data(rtree, "../data/short_trips.csv");
-    //insert_data(rtree, "../data/test.txt");
-    //query1(rtree);
-    //free(rtree);
-    //query2(rtree, 5);
 
+    auto start = system_clock::now();
+    
+    query1(rtree);
+    //query2(rtree, 5);
+    //query3(rtree, Point(-73.922592163085938, 40.754528045654297), Point(-73.91363525390625, 40.765522003173828));
+    
+    auto end = system_clock::now();
+    duration<double> elapsed_seconds = end - start;
+    cout << '\n' << "Elapse time: " << elapsed_seconds.count() << ' s\n';
+    
     //rtree->print_hojas();
-    query3(rtree, Point(-73.922592163085938, 40.754528045654297), Point(-73.91363525390625, 40.765522003173828));
     return 0;
 }
